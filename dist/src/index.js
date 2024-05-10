1d4a4411ca8fc2b3e6db27d6d8ea5cf7f57696fa
Alisherov-044 <alisherov044@gmail.com>
2024-05-10 13:38:14 +0500

    feat: cli command

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,38 +1,32 @@
 import fs from "fs";
 import path from "path";
 import { Command } from "commander";
-
 const program = new Command();
-
-function copyFolderRecursiveSync(source: string, target: string) {
+function copyFolderRecursiveSync(source, target) {
     const files = fs.readdirSync(source);
-
     if (!fs.existsSync(target)) {
         fs.mkdirSync(target);
     }
-
     files.forEach((file) => {
         const currentSource = path.join(source, file);
         const currentTarget = path.join(target, file);
-
         if (fs.statSync(currentSource).isDirectory()) {
             copyFolderRecursiveSync(currentSource, currentTarget);
-        } else {
+        }
+        else {
             fs.copyFileSync(currentSource, currentTarget);
         }
     });
 }
-
-function execute(source: string, target: string) {
+function execute(source, target) {
     console.log(`Copying files to ${target}...`);
     copyFolderRecursiveSync(source, target);
     console.log(`Copying to ${target} complete.`);
 }
-
 program
     .argument("<string>", "folder to initialize project")
     .action((path) => {
-        execute("./template", path);
-    })
+    execute("./template", path);
+})
     .description("Initialize project")
     .parse(process.argv);
