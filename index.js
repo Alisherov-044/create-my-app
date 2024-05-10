9c3b8a87a5fde96a436d37ec3f1b889484023f95
Alisherov-044 <alisherov044@gmail.com>
2024-05-10 15:54:51 +0500

    fix: bin

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+#!/usr/bin/env node
+
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function copyFolderRecursiveSync(source, target) {
     const files = fs.readdirSync(source);
